cd62884e5f199ef5e7ca17c3195109e35989deac
Nghiệm Trần <30282381+woowebsite@users.noreply.github.com>
2020-08-10 00:33:23 +0700

    Progress

--- packages/client/redux/user/sagas.js
+++ packages/client/redux/user/sagas.js
@@ -1,18 +1,17 @@
 import { all, takeEvery, put, call, select } from 'redux-saga/effects'
 import { notification } from 'antd'
 import { JWT_login, JWT_currentAccount, JWT_logout } from 'services/jwt.auth'
-import actions from './actions'
+import constants from './constants'
+import * as actions from './actions'
 
 export function* LOGIN({ payload }) {
   const { email, password } = payload
   const provider = yield select(state => state.settings.authProvider)
   const login = JWT_login
-  yield put({
-    type: 'user/SET_STATE',
-    payload: {
-      loading: true,
-    },
-  })
+  yield put(actions.setState({
+    loading: true
+  }))
+
   const success = yield call(login, email, password)
   yield put({
     type: 'user/LOAD_CURRENT_ACCOUNT',
@@ -30,12 +29,10 @@ export function* LOGIN({ payload }) {
 export function* LOAD_CURRENT_ACCOUNT() {
   const provider = yield select(state => state.settings.authProvider)
   const currentAccount = JWT_currentAccount
-  yield put({
-    type: 'user/SET_STATE',
-    payload: {
-      loading: true,
-    },
-  })
+  yield put(actions.setState({
+    loading: true
+  }))
+  
   const response = yield call(currentAccount)
   if (response) {
     const { uid: id, email, photoURL: avatar } = response
@@ -79,9 +76,9 @@ export function* LOGOUT() {
 
 export default function* rootSaga() {
   yield all([
-    takeEvery(actions.LOGIN, LOGIN),
-    takeEvery(actions.LOAD_CURRENT_ACCOUNT, LOAD_CURRENT_ACCOUNT),
-    takeEvery(actions.LOGOUT, LOGOUT),
+    takeEvery(constants.LOGIN, LOGIN),
+    takeEvery(constants.LOAD_CURRENT_ACCOUNT, LOAD_CURRENT_ACCOUNT),
+    takeEvery(constants.LOGOUT, LOGOUT),
     // LOAD_CURRENT_ACCOUNT(), // run once on app load to check user auth
   ])
 }
