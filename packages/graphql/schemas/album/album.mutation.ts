9d3bd6107aa28b0a14b285b6463bcc030cf6eb00
Nghiệm Trần <30282381+woowebsite@users.noreply.github.com>
2020-09-13 23:20:50 +0700

    Create album: Separate upload file

--- packages/graphql/schemas/album/album.mutation.ts
+++ packages/graphql/schemas/album/album.mutation.ts
@@ -8,24 +8,6 @@ import mkdirp from 'mkdirp'
 const UPLOAD_DIR = './images'
 mkdirp.sync(UPLOAD_DIR)
 
-const storeUpload = ({ stream, filename }) => {
-  const id = shortid.generate()
-  const path = `${UPLOAD_DIR}/${id}-${filename}`
-
-  return new Promise((resolve, reject) =>
-    stream
-      .on('error', error => {
-        if (stream.truncated)
-          // Delete the truncated file.
-          fs.unlinkSync(path)
-        reject(error)
-      })
-      .pipe(fs.createWriteStream(path))
-      .on('error', error => reject(error))
-      .on('finish', () => resolve({ id, path }))
-  )
-}
-
 export const Mutation = {
   createAlbum: rs(Album, {
     before: async (findOptions, args) => {
@@ -42,19 +24,5 @@ export const Mutation = {
       return album;
     },
   }),
-  uploadFile: async (_, { file }) => {
-    const {
-      createReadStream,
-      filename,
-      mimetype,
-      encoding,
-    } = await file;
-
-    const stream = createReadStream()
-
-    const upload: any = await storeUpload({ stream, filename })
-    const { id, path } = upload;
-    return { id, path, filename, mimetype, encoding };
-  },
 
 };
---
+++ packages/graphql/schemas/image/resolver.ts
@@ -0,0 +1,7 @@
+import { Query } from "./image.query";
+import { Mutation } from "./image.mutation";
+
+export const resolver = {
+  // Query: Query,
+  Mutation: Mutation,
+};
