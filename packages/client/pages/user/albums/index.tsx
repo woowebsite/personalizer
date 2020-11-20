df4a455223558d5eb08b4c3a446e3c30d3b30722
Nghiem Tran <nghiem.tran@met>
2020-11-20 16:03:24 +0700

    Temp

--- packages/client/pages/user/albums/index.tsx
+++ packages/client/pages/user/albums/index.tsx
@@ -1,58 +1,77 @@
-import React from "react";
-import MainLayout from "layouts/Main";
-import ListThumbnails from "components/personalizers/ListThumbnails";
-import PAGINGATION from "constants/paginations";
+import React from 'react';
+import MainLayout from 'layouts/Main';
+import ListThumbnails from 'components/personalizers/ListThumbnails';
+import PAGINGATION from 'constants/paginations';
 
 // graphql
-import { withApollo } from "apollo/apollo";
-import { useQuery } from "@apollo/react-hooks";
-import * as queries from "./queries";
+import { withApollo } from 'apollo/apollo';
+import { useQuery } from '@apollo/react-hooks';
+import * as queries from './queries';
 
 //Auth
-import { signIn, signOut, useSession } from 'next-auth/client'
-
-
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 let dataSource: Array<any> = [];
 
 const ManagementAlbums = () => {
-  const { data, loading, error, refetch, fetchMore } = useQuery(queries.GET_ALBUMS, {
-    variables: { where: { userId: 5 }, limit: PAGINGATION.pageSize, offset: 1 }
-  });
+  const [session] = useSession();
+  const { data, loading, error, refetch, fetchMore } = useQuery(
+    queries.GET_ALBUMS,
+    {
+      variables: {
+        where: { userId: 2 },
+        limit: PAGINGATION.pageSize,
+        offset: 0,
+      },
+    }
+  );
 
+  if (loading) return 'Loading...';
+  if (error) return `Error! ${error.message}`;
 
   if (data && data.getAlbums) {
-    dataSource = data.getAlbums
+    dataSource = data.getAlbums;
+
+    if (dataSource[0] && dataSource[0].type != 'action') {
+      dataSource.unshift({
+        type: 'action',
+      });
+    }
   }
 
-  const [session] = useSession()
 
   return (
     <MainLayout>
-      {!session && <>
-        Not signed in <br />
-        <button >Sign in</button>
-      </>}
-      {session && <>
-        Signed in as {JSON.stringify(session)} <br />
-        <button >Sign out</button>
-      </>}
+      {/* {!session && (
+        <>
+          Not signed in <br />
+          <button>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          Signed in as {JSON.stringify(session)} <br />
+          <button>Sign out</button>
+        </>
+      )} */}
 
       <h1>All Albums</h1>
-      {data &&
+      {data && (
         <ListThumbnails
           dataSource={dataSource}
           pagination={{
             onChange: (page) => {
               return refetch({
-                where: { userId: 5 }, limit: PAGINGATION.pageSize, offset: page
+                where: { userId: 2 },
+                limit: PAGINGATION.pageSize,
+                offset: page,
               });
             },
             pageSize: PAGINGATION.pageSize,
-            total: data.getPagination.total
+            total: data.getPagination.total,
           }}
         />
-      }
+      )}
     </MainLayout>
   );
 };
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import nextApp from '@monorepo/client';
 import apolloServer from '@monorepo/graphql';
 
-const PORT = process.env.PORT || '3000';
+const PORT = process.env.PORT || '3001';
 
 async function main() {
   const app = express();
