14c25f736b803039492afc48408d8489342daa1e
Nghiem Tran <nghiem.tran@met>
2020-12-28 14:39:55 +0700

    Remove unused

--- packages/client/pages/login.tsx
+++ packages/client/pages/login.tsx
@@ -1,20 +1,17 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
-import Login from 'components/system/Auth/Login'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Login from 'components/system/Auth/Login';
 // import AuthLayout from "../layouts/Auth";
 
-
 class SystemLogin extends React.Component {
-    render() {
-        return (
-            <div>
-                <Helmet title="Login" />
-                {/* <AuthLayout> */}
-                    <Login />
-                {/* </AuthLayout> */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Helmet title='Login' />
+        <Login />
+      </div>
+    );
+  }
 }
 
-export default SystemLogin
+export default SystemLogin;
